e02190eecdb73f868f32a1cb007bfac95dbb43ee
unknown <aimable.kwizera14@gmail.com>
2024-04-23 18:14:32 +0200

    add swagger documentation

--- index.js
+++ index.js
@@ -2,11 +2,159 @@
 const express = require("express");
 const app = express();
 
+//---------------------------------------------------
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const options = require("./swagger");
+
+//--------------------------------------------------
+
 //route to the books model
 const Book = require("./models/model");
 //import the middle ware to be used
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//******************************************************************************************************************
+
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: The books managing API
+ */
+
+/**
+ * @swagger
+ *   /books:
+ *     get:
+ *       summary: Get all the books
+ *       tags: [Books]
+ *       responses:
+ *         200:
+ *           description: The list of the books
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 items:
+ *                   $ref: '#/components/schemas/Book'
+ *         "404":
+ *           $ref: '#/components/response/404'
+ *         500:
+ *           description: Internal server error
+ */
+
+/**
+ * @swagger
+ *   /books/{id}:
+ *     get:
+ *       summary: Get a book by id
+ *       tags: [Books]
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           schema:
+ *             type: string
+ *           required: true
+ *           description: id of a book
+ *       responses:
+ *         200:
+ *           description: The list of the books
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 items:
+ *                   $ref: '#/components/schemas/Book'
+ *         "404":
+ *           $ref: '#/components/response/404'
+ *         500:
+ *           description: Internal server error
+ */
+
+/**
+ * @swagger
+ *   /books:
+ *     post:
+ *       summary: Add a book
+ *       tags: [Books]
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       responses:
+ *         200:
+ *           description: Book created successfully
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 items:
+ *                   $ref: '#/components/schemas/Book'
+ *         "404":
+ *           $ref: '#/components/response/404'
+ *         500:
+ *           description: Internal server error
+ */
+
+/**
+ * @swagger
+ *   /books/{id}:
+ *     put:
+ *       summary: Update a book
+ *       tags: [Books]
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           schema:
+ *             type: string
+ *           required: true
+ *           description: The ID of the book to update
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       responses:
+ *         200:
+ *           description: Book updated successfully
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Book'
+ *         "404":
+ *           $ref: '#/components/responses/NotFound'
+ *         "500":
+ *           $ref: '#/components/responses/InternalServerError'
+ */
+
+/**
+ * @swagger
+ *   /books/{id}:
+ *     delete:
+ *       summary: Get a book by id
+ *       tags: [Books]
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           schema:
+ *             type: string
+ *           required: true
+ *           description: id of a book
+ *       responses:
+ *         200:
+ *           description: The list of the books
+ *           content:
+ *             application/json:
+ *               type: object
+ *         "404":
+ *           $ref: '#/components/response/404'
+ *         500:
+ *           description: Internal server error
+ */
+
+//******************************************************************************************************************
 
 //create post some data into the db
 app.post("/books", async (req, res) => {
@@ -24,7 +172,7 @@ app.get("/books", async (req, res) => {
     const books = await Book.find({});
     res.status(200).json(books);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(404).json({ message: error.message });
   }
 });
 
@@ -60,19 +208,22 @@ app.delete("/books/:id", async (req, res) => {
     const { id } = req.params;
     const book = await Book.findByIdAndDelete(id);
     if (!book) console.log(`Book with id ${id} not found`);
-    res.status(200).json(book);
+    res.status(200).json({ message: "book deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
+//---------------------------------------------------------------#
+const spacs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(spacs));
+//---------------------------------------------------------------
+
 //import and use mongo db
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(
-    "mongodb+srv://aimable:aimable@cluster0.y5yakew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect("mongodb://127.0.0.1/clasa_db")
   .then(() => {
     console.log("Connected to mongodb successfully");
     app.listen(4000, () => console.log(`Server running on port 4000`));
